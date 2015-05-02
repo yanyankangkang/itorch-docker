37d687a052f869038874df0df5e95fa370496919
kkoba84 <kkoba84@gmail.com>
2015-05-03 00:56:50 +0900

    docker repo is changed

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
-docker run -d -p 9999:9999 -v `pwd`:/root/tutorials/myfolder -it kkoba84/itorch:1.0 /root/torch/install/bin/itorch notebook --profile=itorch_svr
+docker run -d -p 9999:9999 -v `pwd`:/root/tutorials/myfolder -it kkoba84/itorch-docker /root/torch/install/bin/itorch notebook --profile=itorch_svr
 
